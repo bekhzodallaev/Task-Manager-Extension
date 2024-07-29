d1c52b292bd5611df698ba000db1970fc52d3741
Bekhzod Allaev <114129383+bekhzodallaev@users.noreply.github.com>
2024-07-29 20:34:15 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header";
+import "@styles/global.scss";
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <h1>Hello world</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+});
