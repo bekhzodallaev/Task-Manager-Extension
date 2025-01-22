2a41cb1d5bdce1dbb610745b98e0a3913c97b653
Bekhzod Allaev <icebreaker0210mail.ru>
2025-01-22 19:56:21 +0100

    add scrum-8-make-navbar-sticky

--- src/components/FirstPage/FirstPage.jsx
+++ src/components/FirstPage/FirstPage.jsx
@@ -5,7 +5,6 @@ import ReusableBtn from '../ReusableButton/ReusableBtn';
 const FirstPage = () => {
   return (
     <div className={styles.firstPage}>
-      <Header />
       <h1>Stay Organized and Boost Your Productivity</h1>
       <p>Manage tasks effortlessly with our Chrome extension</p>
       <ReusableBtn />
