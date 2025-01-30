8dea1de01bbeb44aac4fec39f2d47de4fb6430a4
Bekhzod Allaev <icebreaker0210mail.ru>
2025-01-30 17:27:46 +0100

    add scrum-19 dashboard field

--- src/components/Dashboard/Header/DashboardHeader.jsx
+++ src/components/Dashboard/Header/DashboardHeader.jsx
@@ -4,12 +4,25 @@ import styles from './DashboardHeader.module.scss';
 function DashboardHeader() {
   return (
     <div className={styles.header_wrapper}>
-      <div>
-        <p>Wednesday, Jan 22 2025</p>
-        <h3>Welcome Bekhzod</h3>
+      <div className={styles.header_texts}>
+        <div className={styles.time_info}>
+          <p>Wednesday, Jan 22 2025</p>
+          <h3>Welcome Bekhzod</h3>
+        </div>
+        <input type="text" placeholder="Search tasks..." />
       </div>
-      <input type="text" placeholder="Search tasks..." />
       <hr />
+      <div className={styles.tasks_list}>
+        <div className={styles.lists_header}>
+          <h2>All Tasks</h2>
+          <div>
+            <p>+</p>
+          </div>
+        </div>
+        <div className={styles.task_placeholder}>
+          <p>Add a task</p>
+        </div>
+      </div>
     </div>
   );
 }
